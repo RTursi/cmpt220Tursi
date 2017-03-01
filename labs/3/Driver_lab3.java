af964a6d19edd86de19e181996b36ad567682671
Joe <Joseph.Tursi1@marist.edu>
2017-02-28 20:16:42 -0500

    4

---
+++ labs/3/Driver_lab3.java
@@ -0,0 +1,23 @@
+/* 
+JOE TURSI
+DR RIVAS
+CMPT220
+LAB3
+*/
+
+public class Driver_lab3{
+  public static void main(String[]args){
+  	Scanner input = new Scanner(System.in);
+  	  while(true){
+  	    int x1 = scan.nextInteger();
+  	      if(x1 == 1)
+  	      	return;
+  	         int x2 = scan.nextInteger();
+  	           int y1 = scan.nextInteger();
+  	            int y2 = scan.nextInteger();
+  	             //#pleasework
+  	                System.out.println(Math.sqrt((x1 -x2)Math.pow(2)+(y1-y2)Math.pow(2));
+  	                  System.out.println();
+  	  }
+  }
+}
